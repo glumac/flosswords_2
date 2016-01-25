0deb0cd33019c249afb1a5f82db75e9287cd362c
glumac <mglumac@gmail.com>
2016-01-25 00:47:25 -0500

    add tooltip with image. TO DO: refactor so tooltip position based on cursor position instead of crossword block position.

--- js/floss.js
+++ js/floss.js
@@ -2,10 +2,12 @@
   var app = angular.module('floss', []);
 
   app.controller('FlossController', ['$http', '$scope', function($http, $scope) {
-    $scope.rows = [],
-    $scope.wide = false,
-    $scope.squareSize,
-    $scope.fontSize; 
+    $scope.rows = [];
+    $scope.wide = false;
+    $scope.squareSize;
+    $scope.fontSize;
+    $scope.tooltipMagnification = 4;
+    $scope.tooltipImg;
 
     $http.get('https://dl.dropboxusercontent.com/u/204045/puzzle.json').then(populatePuzzle, puzzleError);
 
@@ -52,6 +54,49 @@
           puzzleRescale();
 
           $window.on('resize',puzzleRescale);
+        }, 1);
+      }
+    };
+  }]);
+
+  app.directive('tooltip', ['$timeout', function($timeout) {
+    return {
+      restrict: 'A',
+      link: function (scope, elem, attrs) {
+        $timeout(function () {
+          var $tooltip = elem,
+              $currentSquare,
+              currentSquareOffset;
+
+          scope.showTooltip = function(img, $event) {
+            scope.tooltipImg = img;
+
+            $currentSquare = $($event.target);
+
+            setTooltipPosition();
+
+            $tooltip.show();
+          };
+
+          scope.hideTooltip = function() {
+            $tooltip.hide();
+          };
+
+          function setTooltipPosition() {
+            currentSquareOffset = $currentSquare.offset();
+
+            if (currentSquareOffset.left - (scope.squareSize * scope.tooltipMagnification + scope.squareSize * .2) > 0) {
+              $tooltip.css('left', currentSquareOffset.left - (scope.squareSize * scope.tooltipMagnification + scope.squareSize * .2));
+            } else {
+              $tooltip.css('left', currentSquareOffset.left + (scope.squareSize * 1.2));
+            }
+
+            if (currentSquareOffset.top - (scope.squareSize * scope.tooltipMagnification + scope.squareSize * .2) > 0) {
+              $tooltip.css('top', currentSquareOffset.top - (scope.squareSize * scope.tooltipMagnification + scope.squareSize * .2));
+            } else {
+              $tooltip.css('top', currentSquareOffset.top + (scope.squareSize * 1.2));
+            }
+          }
         }, 1);
       }
     };
